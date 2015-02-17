ea76ab457dc1dbbc5e4f774f54fce4f1fea6881d
Matthew Witte <mcw9612@rit.edu>
2015-02-17 08:15:59 -0500

    Style changes to home page and titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
     end
     return search_for
   end
+
+  def title_helper(title = '')
+    main_title = "Vision 2000 & Sunburst Tanning"
+    if title.empty?
+      main_title
+    else
+      "#{title} | #{main_title}"
+    end
+  end
 end
