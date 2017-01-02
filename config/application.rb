898937af89543cc57358ecb431eb702fdc633b0a
Matthew Witte <mcw9612@rit.edu>
2017-01-02 16:43:48 -0500

    Add ckeditor to precompile list

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Vision2000
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.precompile += %w(*.png, *.jpg *.jpeg *.gif)
   end
 end
